8d8087f9fbdc422db4a2f60cb73135f59b6e00da
CrikoC <konstadinoschr@gmail.com>
2018-07-01 18:46:47 +0300

    created show, edit, update and index controls and views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     # Display the parameters that are send by new
     # render plain: params[:article].inspect
@@ -25,8 +29,30 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated!"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def delete
+    @article = Article.find(params[:id])
+
+    if @article.delete(params[:id])
+      flash[:notice] = "Article was successfully deleted!"
+      redirect_to index_path
+    else
+      render 'index'
+    end
+  end
+
   private
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.required(:article).permit(:title, :description)
   end
 end
