d9758bff2d941d21f2643420fa7d9b96abca4075
CrikoC <konstadinoschr@gmail.com>
2018-07-06 12:15:10 +0300

    Added auth restrictions to routes and controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,7 @@ class ArticlesController < ApplicationController
     # render plain: params[:article].inspect
 
     @article = Article.new(article_params)
+    @article.user = current_user
 
     if @article.save
       flash[:success] = "Article was successfully created!"
