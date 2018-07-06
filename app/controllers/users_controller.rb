fd272a3652865a69d1f7843dd3b3ae209eb361f6
CrikoC <konstadinoschr@gmail.com>
2018-07-06 12:22:05 +0300

    Automatic signin upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
-  before_action :require_same_user only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -21,8 +21,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "User was successfully registered!"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
